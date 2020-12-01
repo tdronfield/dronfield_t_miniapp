9659e124825075eb255865e20aa36a018ffc89bd
Taylor Dronfield <t_dronfield@fanshaweonline.ca>
2020-12-01 12:15:08 -0500

    trying to debug click to show bio

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { fetchData } from "./components/TheDataMiner.js";
-import CarCard from "./components/TheCarCard.js";
+import TheCarCard from "./components/TheCarCard.js";
 
 (() => {
    
@@ -8,8 +8,7 @@ import CarCard from "./components/TheCarCard.js";
         // el: "#app",
 
         data: {
-            message: "Hello from Vue",
-            removeAformat: true,
+            
             showBioData: false,
             collection: [],
             currentItemData: {},
@@ -34,8 +33,6 @@ import CarCard from "./components/TheCarCard.js";
                     
                 })
                 .catch(err => console.error(err));
-
-
         },
 
         // "Updated" is it's own lifecycle Hook.
@@ -44,43 +41,35 @@ import CarCard from "./components/TheCarCard.js";
             console.log("Vue updated the DOM");
         },
 
-        methods: {
-            logClicked() {
-                console.log("Clicks a thing");
-            },
-
-            clickHeader(){
-                console.log("Clicked the header");
-            },
-
-            showProfData(target){
-                console.log("Clicked to view Car info", target, target.name);
-                //the this keyword inside a vue instance REFERS to the vue instance itself by default
-
-                // toggle the property between true and false using a ternary statement
-                this.showBioData = this.showBioData ? false: true;
-
-                // make the selected profs data visible
-                this.currentItemData = target;
-
-            },
-
-            removeProf(target){
-                console.log('Clicked to remove Car', target, target.name);
-                // THIS keyword refers to VUE
-
-                // make the selected prof's data be removed from the array
-                // POP the last obj out of the array, then return the rest of the remaining objects 
-                // SPLICE method
-                this.collection.splice(this.collection.indexOf(target), 1);
-
-                // delete method, not working for me
-                //this.$delete(this.collection, target);
-            }
-        },
+        // methods: {
+        //     showProfData(target){
+        //         console.log("Clicked to view Car info", target, target.name);
+        //         //the this keyword inside a vue instance REFERS to the vue instance itself by default
+    
+        //         // toggle the property between true and false using a ternary statement
+        //         this.showBioData = this.showBioData ? false: true;
+    
+        //         // make the selected profs data visible
+        //         this.currentItemData = target;
+    
+        //     },
+    
+            // removeProf(target){
+            //     console.log('Clicked to remove Car', target, target.name);
+            //     // THIS keyword refers to VUE
+    
+            //     // make the selected prof's data be removed from the array
+            //     // POP the last obj out of the array, then return the rest of the remaining objects 
+            //     // SPLICE method
+            //     this.collection.splice(this.collection.indexOf(target), 1);
+    
+            //     // delete method, not working for me
+            //     //this.$delete(this.collection, target);
+            // }
+        // },
 
         components: {
-            "car-card": CarCard
+            "car-card": TheCarCard
         } 
     }).$mount("#app"); // also connects Vue to your wrapper in HTML
 
